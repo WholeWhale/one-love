cb7001dcb98637917f53a6069a5d5a993d065586
Chris Johnson <chris@cjcodes.com>
2017-05-09 17:29:53 -0400

    Change deploy message for slack

--- bin/deploy_to_pantheon.sh
+++ bin/deploy_to_pantheon.sh
@@ -16,7 +16,7 @@ COMMIT_MESSAGE="$(git show --name-only --decorate)"
 PANTHEON_ENVS="$(terminus multidev:list $PANTHEON_SITE_UUID --format=list --field=Name)"
 GITHUB_API_URL="https://api.github.com/repos/$CIRCLE_PROJECT_USERNAME/$CIRCLE_PROJECT_REPONAME"
 PANTHEON_SITE_NAME="$(terminus site:info $PANTHEON_SITE_UUID --fields=name --format=string)"
-SLACK_MESSAGE="Circle CI build ${CIRCLE_BUILD_NUM} by ${CIRCLE_PROJECT_USERNAME} was successful and has been deployed to Pantheon on <https://dashboard.pantheon.io/sites/${PANTHEON_SITE_UUID}#dev/code|the dev environment>! \nTo deploy to test run "'`terminus env:deploy '"${PANTHEON_SITE_UUID}"'.test`'" or merge from <https://dashboard.pantheon.io/sites/${PANTHEON_SITE_UUID}#test/deploys|the site dashboard>."
+SLACK_MESSAGE=""
 
 cd $HOME
 
@@ -46,7 +46,7 @@ terminus auth:login --machine-token=$PANTHEON_MACHINE_TOKEN
 # Stash site name
 PANTHEON_SITE_NAME="$(terminus site:info $PANTHEON_SITE_UUID --fields=name --format=string)"
 
-SLACK_MESSAGE="Circle CI build ${CIRCLE_BUILD_NUM} by ${CIRCLE_PROJECT_USERNAME} was successful and has been deployed to Pantheon on <https://dashboard.pantheon.io/sites/${PANTHEON_SITE_UUID}#dev/code|the dev environment>! \nTo deploy to test run "'`terminus env:deploy '"${PANTHEON_SITE_UUID}"'.test`'" or merge from <https://dashboard.pantheon.io/sites/${PANTHEON_SITE_UUID}#test/deploys|the site dashboard>."
+SLACK_MESSAGE="Circle CI build ${CIRCLE_BUILD_NUM} by ${CIRCLE_PROJECT_USERNAME} was successful and has been deployed to Pantheon in the dev environment! \n<https://dev-$PANTHEON_SITE_NAME.pantheonsite.io/|View dev site> | <https://dashboard.pantheon.io/sites/${PANTHEON_SITE_UUID}#dev/code|View on Pantheon Dashboard>"
 
 # Check if we are NOT on the master branch and this is a PR
 if [[ $CIRCLE_BRANCH != "master" && -n "$CI_PULL_REQUEST" ]]
