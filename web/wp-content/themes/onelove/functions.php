39dd908c11011e267bf9a6dc67444672e6b41f68
Luis Delacruz <luis@luisdelacruz.io>
2017-06-07 13:46:22 -0400

    allow svg uploads to media library

--- web/wp-content/themes/onelove/functions.php
+++ web/wp-content/themes/onelove/functions.php
@@ -3,5 +3,13 @@
 function enqueue_styles() {
   wp_enqueue_style( 'main-style', get_stylesheet_directory_uri() . '/assets/stylesheets/onelove.css' );
 }
-
 add_action( 'wp_enqueue_scripts', 'enqueue_styles' );
+
+/**
+ * Allow loading of svg images within media library
+ */
+function custom_upload_mimes ( $existing_mimes=array() ) {
+    $existing_mimes['svg'] = 'mime/type';
+    return $existing_mimes;
+}
+add_filter('upload_mimes', 'custom_upload_mimes');
