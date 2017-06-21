4b776470f7313b6ba02b6da475a0def89f1d6639
Luis Delacruz <luis@luisdelacruz.io>
2017-06-21 12:57:07 -0400

    create unique categories for each custom post type

--- web/wp-content/themes/onelove/functions.php
+++ web/wp-content/themes/onelove/functions.php
@@ -64,3 +64,44 @@ function register_meta_boxes() {
   }
 }
 add_action('after_setup_theme', 'register_meta_boxes');
+
+function unique_categories() {
+
+  $args = array(
+       'public'   => true,
+       '_builtin' => false,
+    );
+
+
+    $post_types = get_post_types( $args, 'names', 'and' );
+
+    foreach ( $post_types  as $post_type ) {
+
+      // create a new taxonomy
+      $labels = array(
+    		'name'              => _x( 'Categories', 'taxonomy general name', 'textdomain' ),
+    		'singular_name'     => _x( 'Category', 'taxonomy singular name', 'textdomain' ),
+    		'search_items'      => __( 'Search Categories', 'textdomain' ),
+    		'all_items'         => __( 'All Categories', 'textdomain' ),
+    		'parent_item'       => __( 'Parent Category', 'textdomain' ),
+    		'parent_item_colon' => __( 'Parent Category:', 'textdomain' ),
+    		'edit_item'         => __( 'Edit Categories', 'textdomain' ),
+    		'update_item'       => __( 'Update Category', 'textdomain' ),
+    		'add_new_item'      => __( 'Add New Category', 'textdomain' ),
+    		'new_item_name'     => __( 'New Category', 'textdomain' ),
+    		'menu_name'         => __( 'Categories', 'textdomain' ),
+    	);
+      $post_type_base_name = preg_replace( '/_post_type/','',$post_type );
+      register_taxonomy(
+        $post_type_base_name.'_category',
+        $post_type,
+        array(
+          'labels'             => $labels,
+          'publicly_queryable' => true,
+          'rewrite'            => array( 'slug' => $post_type_base_name .'-category' ),
+        )
+      );
+    }
+
+}
+add_action( 'init', 'unique_categories' );
