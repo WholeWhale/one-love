acefe7982afe1681a9e4db990a61d25dfb9f985e
Luis Delacruz <luis@luisdelacruz.io>
2017-06-24 20:12:29 -0400

    update menu styling

--- web/wp-content/themes/onelove/library/navigation.php
+++ web/wp-content/themes/onelove/library/navigation.php
@@ -6,7 +6,7 @@ function foundationpress_mobile_nav() {
     'menu'           => __( 'mobile-nav', 'foundationpress' ),
     'menu_class'     => 'vertical menu',
     'theme_location' => 'mobile-nav',
-    'items_wrap'     => '<ul id="%1$s" class="%2$s" data-accordion-menu>%3$s</ul>'.get_search_form(false),
+    'items_wrap'     => '<ul id="%1$s" class="%2$s" data-accordion-menu>%3$s</ul>'.get_search_form(false).do_shortcode('[social_media]'),
     'fallback_cb'    => false,
     'walker'         => new Foundationpress_Mobile_Walker(),
   ));
