10ff087589fbf3bd0f9160be736d7ed9d299e676
Luis Delacruz <luis@luisdelacruz.io>
2017-06-25 20:24:38 -0400

    update styling of default card; some card styling of vc grid

--- web/wp-content/themes/onelove/functions.php
+++ web/wp-content/themes/onelove/functions.php
@@ -112,22 +112,3 @@ add_action( 'init', 'unique_categories' );
 foreach (glob('{'. get_stylesheet_directory() . '/library/*.php,'.get_stylesheet_directory() . '/library/vc-addons/*.addon.php}',GLOB_BRACE) as $file) {
   include $file;
 }
-
-
-
-add_filter( 'vc_grid_item_shortcodes', 'my_module_add_grid_shortcodes' );
-function my_module_add_grid_shortcodes( $shortcodes ) {
-   $shortcodes['vc_say_hello'] = array(
-     'name' => __( 'Say Hello', 'my-text-domain' ),
-     'base' => 'vc_say_hello',
-     'category' => __( 'Content', 'my-text-domain' ),
-     'description' => __( 'Just outputs Hello World', 'my-text-domain' ),
-     'post_type' => Vc_Grid_Item_Editor::postType(),
-  );
-   return $shortcodes;
-}
-
-add_shortcode( 'vc_say_hello', 'vc_say_hello_render' );
-function vc_say_hello_render() {
-   return '<h2>Hello, World!</h2>';
-}
---
+++ web/wp-content/themes/onelove/library/vc-addons/shortcodes.addon.php
@@ -0,0 +1,20 @@
+<?php
+
+// sample
+
+// add_filter( 'vc_grid_item_shortcodes', 'my_module_add_grid_shortcodes' );
+// function my_module_add_grid_shortcodes( $shortcodes ) {
+//    $shortcodes['vc_say_hello'] = array(
+//      'name' => __( 'Say Hello', 'my-text-domain' ),
+//      'base' => 'vc_say_hello',
+//      'category' => __( 'Content', 'my-text-domain' ),
+//      'description' => __( 'Just outputs Hello World', 'my-text-domain' ),
+//      'post_type' => Vc_Grid_Item_Editor::postType(),
+//   );
+//    return $shortcodes;
+// }
+//
+// add_shortcode( 'vc_say_hello', 'vc_say_hello_render' );
+// function vc_say_hello_render() {
+//    return '<h2>Hello, World!</h2>';
+// }
