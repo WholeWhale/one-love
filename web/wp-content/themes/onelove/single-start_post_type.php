6a48edf1cd73649390d77eb7ab142f0b171a6f4e
Luis Delacruz <luis@luisdelacruz.io>
2017-06-27 07:25:03 -0400

    add fallback when image is not uploaded to start template

--- web/wp-content/themes/onelove/single-start_post_type.php
+++ web/wp-content/themes/onelove/single-start_post_type.php
@@ -9,6 +9,11 @@
 get_header();
 
 
+
+if (has_post_thumbnail( $post->ID )) {
+
+
+
 $button_modal = get_post_meta(get_the_ID(),'campaign-popup','true');
 $button_text  = get_post_meta(get_the_ID(),'campaign-button-cta','true');
 $description  = get_post_meta(get_the_ID(),'campaign-description','true');
@@ -66,6 +71,16 @@ get_template_part( 'template-parts/featured-image' );
     });
   </script>
 
+<?php }
+else { ?>
+
+
+  <div id="single-post" role="main">
+    <h1 class="entry-title" style="margin-top: 30px;"><?php the_title(); ?></h1>
+<?php
+}
+
+?>
 
 <?php do_action( 'foundationpress_before_content' ); ?>
 <?php while ( have_posts() ) : the_post(); ?>
