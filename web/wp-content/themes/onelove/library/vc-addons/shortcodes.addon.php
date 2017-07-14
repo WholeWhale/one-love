7a67a9cda552b72b5fa179e9fdaf68afa85a2ad7
Luis Delacruz <luis@luisdelacruz.io>
2017-07-14 13:28:28 -0400

    create an advance category shortcode

--- web/wp-content/themes/onelove/library/vc-addons/shortcodes.addon.php
+++ web/wp-content/themes/onelove/library/vc-addons/shortcodes.addon.php
@@ -19,6 +19,166 @@
 //    return '<h2>Hello, World!</h2>';
 // }
 
+
+
+add_filter( 'vc_grid_item_shortcodes', 'categories_enhanced_add_grid_shortcode' );
+function categories_enhanced_add_grid_shortcode( $shortcodes ) {
+   $shortcodes['vc_categories_enhanced'] = array(
+      'name'        => __( 'Post Categories: Enhanced', 'my-text-domain' ),
+      'base'        => 'vc_categories_enhanced',
+      'icon'        => 'vc_icon-vc-gitem-post-categories',
+      'category'    => __( 'Post', 'my-text-domain' ),
+      'description' => __( 'Categories of current post with extra features', 'my-text-domain' ),
+      'params'      => array(
+        array(
+          'type'        => 'checkbox',
+          'heading'     => 'Add Link',
+          'param_name'  => 'link',
+          'value'       => '',
+          'description' => 'Add link to category?'
+        ),
+        array(
+          'type'        => 'dropdown',
+          'heading'     => 'Style',
+          'param_name'  => 'category_style',
+          'value'       => array(
+            'none'                => '',
+            'Comma'               => ',',
+            'Rounded'             => 'filled vc_grid-filter-filled-round-all',
+            'Less Rounded'        => 'filled vc_grid-filter-filled-rounded-all',
+            'Border'              => 'bordered',
+            'Rounded Border'      => 'bordered-rounded vc_grid-filter-filled-round-all',
+            'Less Rounded Border' => 'bordered-rounded-less vc_grid-filter-filled-rounded-all',
+          ),
+          'description' => 'Select category display style.'
+        ),
+        array(
+          'type'       => 'dropdown',
+          'heading'    => 'Color',
+          'param_name' => 'category_color',
+          'value'      => array(
+            'Blue'        => 'blue',
+            'Turquoise'   => 'turquoise',
+            'Pink'        => 'pink',
+            'Violet'      => 'violet',
+            'Peacoc'      => 'peacoc',
+            'Chino'       => 'chino',
+            'Mulled Wine' => 'mulled_wine',
+            'Vista Blue'  => 'vista_blue',
+            'Black'       => 'black',
+            'Grey'        => 'grey',
+            'Orange'      => 'orange',
+            'Sky'         => 'sky',
+            'Green'       => 'green',
+            'Juicy pink'  => 'juicy_pink',
+            'Sandy brown' => 'sandy_brown',
+            'Purple'      => 'purple',
+            'White'       => 'white',
+            'Coral'       => 'coral',
+          ),
+          'std' => 'coral',
+          'param_holder_class' => 'vc_colored-dropdown',
+        ),
+        array(
+  				'type'        => 'dropdown',
+  				'heading'     => __( 'Category size', 'js_composer' ),
+  				'param_name'  => 'category_size',
+  				'value'       => getVcShared( 'sizes' ),
+  				'std'         => 'md',
+  				'description' => __( 'Select category size.', 'js_composer' ),
+  			),
+  			array(
+  				'type'        => 'textfield',
+  				'heading'     => __( 'Extra class name', 'js_composer' ),
+  				'param_name'  => 'el_class',
+  				'description' => __( 'Style particular content element differently - add a class name and refer to it in custom CSS.', 'js_composer' ),
+  			),
+  			array(
+  				'type'       => 'css_editor',
+  				'heading'    => __( 'CSS box', 'js_composer' ),
+  				'param_name' => 'css',
+  				'group'      => __( 'Design Options', 'js_composer' ),
+  			),
+      ),
+      'post_type' => Vc_Grid_Item_Editor::postType(),
+   );
+
+   return $shortcodes;
+}
+// output function
+add_shortcode( 'vc_categories_enhanced', 'vc_categories_enhanced_render' );
+function vc_categories_enhanced_render($atts) {
+   return '{{ categories_enhanced:'.http_build_query( (array) $atts ).' }}';
+}
+
+add_filter( 'vc_gitem_template_attribute_categories_enhanced', 'vc_gitem_template_attribute_categories_enhanced', 10, 2 );
+function vc_gitem_template_attribute_categories_enhanced( $value, $data ) {
+   /**
+    * @var Wp_Post $post
+    * @var string $data
+    */
+    extract( array_merge( array(
+   		'post' => null,
+   		'data' => '',
+   	), $data ) );
+   	$atts_extended = array();
+   	parse_str( $data, $atts_extended );
+
+    $atts = $atts_extended['atts'];
+
+    VcShortcodeAutoloader::getInstance()->includeClass( 'WPBakeryShortCode_VC_Gitem_Post_Categories_Enhanced' );
+
+    $post = get_post( $post->ID );
+    $post_type = $post->post_type;
+    $taxonomies = get_object_taxonomies( $post_type, 'objects' );
+
+    $separator = '';
+    $css_class = array( 'vc_gitem-post-data' );
+    $css_class[] = vc_shortcode_custom_css_class( $atts['css'] );
+    $css_class[] = $atts['el_class'];
+    $css_class[] = 'vc_gitem-post-data-source-post_categories';
+    $style = str_replace( ',', 'comma', $atts['category_style'] );
+    $output = '<div class="' . esc_attr( implode( ' ', array_filter( $css_class ) ) ) . ' vc_grid-filter vc_clearfix vc_grid-filter-' . esc_attr( $style ) . ' vc_grid-filter-size-' . esc_attr( $atts['category_size'] ) . ' vc_grid-filter-center vc_grid-filter-color-' . esc_attr( $atts['category_color'] ) . '">';
+    $data = array();
+
+
+    foreach ( $taxonomies as $taxonomy_slug => $taxonomy ) {
+      // Get the terms related to post.
+      $categories = get_the_terms( $post->ID, $taxonomy_slug );
+      if ( ! empty( $categories ) ) {
+      	foreach ( $categories as $category ) {
+
+
+      		$category_link = '';
+      		if ( ! empty( $atts['link'] ) ) {
+      			$category_link = 'href="' . esc_url( get_category_link( $category->term_id ) ) . '" alt="' . esc_attr( sprintf( __( 'View all posts in %s', 'js_composer' ), $category->name ) ) . '"';
+      		}
+
+      		$wrapper = '<div class="vc_grid-filter-item vc_gitem-post-category-name">';
+      		$content = esc_html( $category->name );
+      		if ( ! empty( $category_link ) ) {
+      			$content = '<h5 class="vc_gitem-post-category-name"><a ' . $category_link . ' class="vc_gitem-link">' . $content . '</a>' . '</h5>';
+      		} else {
+      			$content = '<h5 class="vc_gitem-post-category-name">' . $content . '</h5>';
+      		}
+      		$wrapper_end = '</div>';
+      		$data[] = $wrapper . $content . $wrapper_end;
+
+
+      	}
+      }
+    }
+
+    if ( empty( $atts['category_style'] ) || ' ' === $atts['category_style'] || ', ' === $atts['category_style'] ) {
+    	$separator = $atts['category_style'];
+    }
+    $output .= implode( $separator, $data );
+    $output .= '</div>';
+
+    return $output;
+}
+
+
 add_action( 'vc_before_init', 'card_component' );
 
 function card_component() {
