b6bfe3da706984c52c6839c11c8f8c2cbf103f4d
Luis Delacruz <luis@luisdelacruz.io>
2017-07-15 01:58:32 -0400

    last name metabox

--- web/wp-content/themes/onelove/functions.php
+++ web/wp-content/themes/onelove/functions.php
@@ -292,3 +292,15 @@ function inject_gtm_body_script() {
   }
 }
 add_action('foundationpress_after_body','inject_gtm_body_script');
+
+
+function check_metadata() {
+  global $post;
+  $myvals = get_post_meta($post->ID);
+
+  foreach($myvals as $key=>$val)
+  {
+      echo $key . ' : ' . $val[0] . '<br/>';
+  }
+}
+add_shortcode('check_metadata','check_metadata');
