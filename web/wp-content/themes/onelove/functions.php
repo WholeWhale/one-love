720cab7c4e9150edea44e647787d10fd7a28b787
Luis Delacruz <luis@luisdelacruz.io>
2017-07-16 13:34:05 -0400

    modify dimensions of featured images

--- web/wp-content/themes/onelove/functions.php
+++ web/wp-content/themes/onelove/functions.php
@@ -304,3 +304,11 @@ function check_metadata() {
   }
 }
 add_shortcode('check_metadata','check_metadata');
+
+function add_additional_image_sizes() {
+  add_image_size( 'featured-alt-small', 640, 400, true ); // name, width, height, crop
+  add_image_size( 'featured-alt-medium', 1280, 600, true );
+  add_image_size( 'featured-alt-large', 1440, 600, true );
+  add_image_size( 'featured-alt-xlarge', 1920, 600, true );
+}
+add_action('after_setup_theme','add_additional_image_sizes');
