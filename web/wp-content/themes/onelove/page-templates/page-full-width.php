301cae53ac4cca8e1a28704fa4e4ff7c591447e9
Luis Delacruz <luis@luisdelacruz.io>
2017-08-08 15:10:24 -0400

    update styling of full page header template when there is no featured image

--- web/wp-content/themes/onelove/page-templates/page-full-width.php
+++ web/wp-content/themes/onelove/page-templates/page-full-width.php
@@ -20,17 +20,20 @@ $subtitle = get_post_meta(get_the_ID(),'subtitle','true');
       </header>
     <?php endif; ?>
 
-
+    <?php if ( !has_post_thumbnail( $post->ID ) ): ?>
+      <div class="full-width-no-featured-image navy">
+        <div>
+          <h1 class="entry-title"><?php the_title(); ?></h1>
+          <h3><?php echo $subtitle; ?></h3>
+        </div>
+      </div>
+    <?php endif; ?>
 <div id="page-full-width" role="main">
 
 <?php do_action( 'foundationpress_before_content' ); ?>
 <?php while ( have_posts() ) : the_post(); ?>
   <article <?php post_class('main-content') ?> id="post-<?php the_ID(); ?>">
       <?php do_action( 'foundationpress_page_before_entry_content' ); ?>
-      <?php if ( !has_post_thumbnail( $post->ID ) ): ?>
-        <h1 class="entry-title"><?php the_title(); ?></h1>
-        <h3><?php echo $subtitle; ?></h3>
-      <?php endif; ?>
       <div class="entry-content">
           <?php the_content(); ?>
       </div>
