56e9b2ad8fe58eebe499d68672b18094d5a97ab0
Luis Delacruz <luis@luisdelacruz.io>
2017-08-22 06:38:13 -0400

    remove footer margin on full page template; increase z index of first post-convo div

--- web/wp-content/themes/onelove/page-templates/page-full-width.php
+++ web/wp-content/themes/onelove/page-templates/page-full-width.php
@@ -47,20 +47,6 @@ $subtitle = get_post_meta(get_the_ID(),'subtitle','true');
       <div class="entry-content">
           <?php the_content(); ?>
       </div>
-      <footer>
-          <?php
-            wp_link_pages(
-              array(
-                'before' => '<nav id="page-nav"><p>' . __( 'Pages:', 'foundationpress' ),
-                'after'  => '</p></nav>',
-              )
-            );
-          ?>
-          <p><?php the_tags(); ?></p>
-      </footer>
-      <?php do_action( 'foundationpress_page_before_comments' ); ?>
-      <?php comments_template(); ?>
-      <?php do_action( 'foundationpress_page_after_comments' ); ?>
   </article>
 <?php endwhile;?>
 
