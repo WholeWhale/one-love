30f08cf68e6ac219bbff26b6325b0049058059db
Luis Delacruz <luis@luisdelacruz.io>
2017-08-23 16:31:51 -0400

    fix membership card overlaying sibling

--- web/wp-content/themes/onelove/assets/javascript/custom/membership-card-home.js
+++ web/wp-content/themes/onelove/assets/javascript/custom/membership-card-home.js
@@ -3,14 +3,15 @@
 
     if (typeof $('.vc-membership-card') !== 'undefined') {
       $('.vc-membership-card').each(function(i){
-        calculate_margin($(this));
-        $(window).resize(function(){calculate_margin($(this));});
+        calculate_margin($('.vc-membership-card').eq(i));
+        $(window).resize(function(){calculate_margin($('.vc-membership-card').eq(i));});
       });
     }
   }
   function calculate_margin(s) {
     var halfHeight = (s.outerHeight()/2);
-    s.parent().css('margin-top',halfHeight-67.5);
+    var newMargin = (halfHeight - 16 - 50);
+    s.parents('.vc_column-inner').css('margin-top',newMargin);
     s.parents('.vc_row').prevAll('.vc_row').first().css('padding-bottom',halfHeight);
   }
 })(jQuery);
