97b09ef63ff6e7e63f0567d09bb11bd1dc0a02a3
Luis Delacruz <luis@luisdelacruz.io>
2017-08-28 12:30:28 -0400

    track pardot form submissions

--- web/wp-content/themes/onelove/assets/javascript/custom/recieve-pardot-data.js
+++ web/wp-content/themes/onelove/assets/javascript/custom/recieve-pardot-data.js
@@ -3,6 +3,7 @@ window.addEventListener('message',function(event){
 
   displayResult('first_name','.membership-card-firstName');
   displayResult('last_name','.membership-card-lastName');
+  getFormSubmit();
 
   function displayResult(s,t) {
     var eventData = event.data.info;
@@ -11,6 +12,15 @@ window.addEventListener('message',function(event){
       if (name > -1) {
         document.activeElement.parentNode.parentNode.querySelectorAll(t)[0].innerHTML = event.data.info.substr(event.data.info.indexOf(":") + 1);
       }
+    }
+  }
+
+  function getFormSubmit() {
+    var formSubmission = event.data.submit;
+    if (typeof formSubmission !== 'undefined') {
+      dataLayer.push({
+        'submit': 'pardot form'
+      });
     }
   }
 
