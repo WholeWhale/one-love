f84a34b6874aeea87e954bba0323687469752a62
Chris Johnson <chris@cjcodes.com>
2018-01-09 12:48:45 -0500

    Add a quick quit for autodiscover route.

--- web/index.php
+++ web/index.php
@@ -2,5 +2,10 @@
 // Tells WordPress to load the WordPress theme and output it.
 define('WP_USE_THEMES', true);
 
-// Loads the WordPress Environment and Template
-require( dirname( __FILE__ ) . '/wp/wp-blog-header.php' );
+if ($_SERVER['REQUEST_URI'] === '/autodiscover/autodiscover.xml') {
+  http_response_code(401);
+} else {
+  // Loads the WordPress Environment and Template
+  require( dirname( __FILE__ ) . '/wp/wp-blog-header.php' );
+}
+
